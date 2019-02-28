d6428c7d8bef1795b9f2b8afe01df79ab955c7a8
Aron Nicholasson <aronmar@live.no>
2019-02-28 11:13:34 +0100

    UI and assets update
    ### Added
    - Example scene
    - Add-Ons assets for tiles and brushes
    - Sprites
    - Path Rule-tile
    - Created dynamic panels ComputerPanel & ModulePanel
    - Made panels appear on click
    - Created a starting GameManager, Grid (with tilemaps) and canvas

---
+++ Assets/Scripts/DesignPatterns/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A generic singleton class for creating singleton
+/// </summary>
+/// <typeparam name="T">The type of MonoBehaviour the singleton needs to be</typeparam>
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+
+    /// The instance of the singleton
+    private static T instance;
+
+    /// Property for accessing the singleton
+    public static T Instance {
+        get {
+            if (instance == null) {
+                instance = FindObjectOfType<T>();
+            }
+            //Returns the instance
+            return instance;
+        }
+
+    }
+}
---
+++ Assets/Scripts/Settings/Settings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject settingsPanel;
+
+    [SerializeField]
+    private Button exitBtn;
+
+    [SerializeField]
+    private Button continueBtn;
+
+    [SerializeField]
+    private Button fullscreenBtn;
+
+    [SerializeField]
+    private Button helpBtn;
+
+    [SerializeField]
+    private Button concedeBtn;
+
+    [SerializeField]
+    private Slider musicSlider;
+
+    [SerializeField]
+    private Slider soundSlider;
+
+    /// <summary>
+    /// On mouseClick open settings menu
+    /// </summary>
+    public void OnMouseDown () {
+        this.settingsPanel.SetActive (!this.settingsPanel.activeSelf);
+    }
+
+}
