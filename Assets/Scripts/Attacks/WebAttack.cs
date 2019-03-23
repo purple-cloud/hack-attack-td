f34c9c4e31e75cc881437b6dc766c1573bf86fd9
Aron Nicholasson <aronmar@live.no>
2019-03-23 14:21:42 +0100

    WIP: Create level 1

---
+++ Assets/Scripts/Attacks/WebAttack.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebAttack : Pathfinder {
+
+    private int port;
+
+    private bool isAttackable = false;
+
+    private void Start() {
+        
+    }
+
+    /// <summary>
+    /// Finds a game object to attack
+    /// </summary>
+    public void FindInfectableGameObject() {
+        while (!this.isAttackable) {
+            if (ScanComponent(SelectedComponent)) {
+                this.isAttackable = true;
+            } else {
+                MoveToNextOutput();
+            }
+        }
+        AttackGameObject();
+    }
+
+    public void AttackGameObject() {
+        Debug.Log("Attacking game object: " + SelectedComponent.name);
+    }
+
+    /// <summary>
+    /// Create some sort of check if time has expired. if so, delete attack object
+    /// this to prevent overflow in worst case....
+    /// </summary>
+    public void hasTimerExpired() {
+
+    }
+
+    /// <summary>
+    /// Create some sort of timer that starts when attack object is born
+    /// </summary>
+    /// <param name="newTime"></param>
+    public void NextTimer(int newTime) {
+
+    }
+
+    /// <summary>
+    /// deletes this attack object
+    /// </summary>
+    private void DeleteAttack() {
+
+    }
+
+    /// <summary>
+    /// Scans the specified component
+    /// </summary>
+    /// <param name="componentToScan">component to scan</param>
+    public bool ScanComponent(Component componentToScan) {
+        if (componentToScan.GetComponent(typeof(Component)).GetType() == typeof(Firewall)) {
+            //TODO Create an if statement checking if the firewall has enabled or disabled access for this attacks port
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Components/Earth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Earth : Component {
+
+    /// <summary>
+    /// Spawn x number of enemies with n interval
+    /// </summary>
+    public void SpawnEnemies() {
+        Debug.Log("cunt");
+    }
+
+    /// <summary>
+    /// Starts the wave of enemies after a certain time has passed
+    /// </summary>
+    public void CheckIfTimePassed() {
+
+    }
+
+}
