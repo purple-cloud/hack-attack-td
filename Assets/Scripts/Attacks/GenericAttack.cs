922b744efa3e2262eabac49e50294e4482ef9367
Jenny <jenny.skjeret@gmail.com>
2019-04-02 14:31:03 +0200

    Generic attack class

---
+++ Assets/Scripts/Attacks/GenericAttack.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class GenericAttack : Pathfinder {
+
+    private string name;
+
+    private int port;
+
+    private bool isAttackable = false;
+
+    private bool repeatAttack = false;
+
+    public GenericAttack(Component initialComponent) : base(initialComponent) {
+
+    }
+
+    private void AttackComponent() {
+        // Each attack does 10 dmg to the durability
+        SelectedComponent.Durability -= 10;
+        Debug.Log("Durability left: " + SelectedComponent.Durability);
+        // TODO Update Module panel with new Computer Durability Values
+    }
+
+    /// <summary>
+    /// deletes this attack object
+    /// </summary>
+    private void DeleteAttack() {
+        Debug.Log("Deleting attack..");
+        Destroy(this.gameObject);
+    }
+
+    /// <summary>
+    /// Scans the specified component and returns true if computer component
+    /// is found and false if not
+    /// </summary>
+    /// <param name="componentToScan"></param>
+    /// <returns>true if computer component is found and false if not</returns>
+    private bool ScanComponent(Component componentToScan, Component componentToFind) {
+        bool componentFound = false;
+        try {
+            if (componentToScan.GetType() == componentToFind.GetType()) {
+                componentFound = true;
+            } else if (componentToScan.GetType() == typeof(Firewall)) {
+                //TODO Change this check to find the status of port in real firewall script (Wait for liban to finish firewall)
+                Firewall firewall = (Firewall) componentToScan;
+                bool portStatus = firewall.GetPort(this.port).IsActive;
+                Debug.Log("Selected component is a firewall, port status: " + portStatus);
+                if (!portStatus) {
+                    if (componentToScan.GetType() == componentToFind.GetType()) {
+                        componentFound = true;
+                    }
+                } else {
+                    componentFound = false;
+                    DeleteAttack();
+                }
+            } else {
+                Debug.Log("Component is of type: " + componentToScan.Name);
+            }
+        } catch (NullReferenceException nre) {
+            Debug.LogException(nre);
+        }
+        return componentFound;
+    }
+
+    /// <summary>
+    /// Returns the name of the attack. Could be domain, location etc.
+    /// </summary>
+    /// <returns>Returns the name of the attack</returns>
+    public string GenericAttackName() {
+        return this.name;
+    }
+
+    /// <summary>
+    /// Finds a game object to attack
+    /// </summary>
+    public Component FindAttackableGameObject(Component component) {
+        Component foundComponent = null;
+        try {
+            while (!this.isAttackable) {
+                if (ScanComponent(SelectedComponent, component)) {
+                    this.isAttackable = true;
+                    foundComponent = SelectedComponent;
+                } else {
+                    MoveToNextOutput();
+                }
+            }
+        } catch (NullReferenceException e) {
+            Debug.LogException(e);
+        }
+        return foundComponent;
+    }
+
+
+
+}
--- Assets/Scripts/Attacks/WebAttack.cs
+++ Assets/Scripts/Attacks/WebAttack.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class WebAttack : Pathfinder {
+public class WebAttack : GenericAttack {
 
     private string name;
 
@@ -20,99 +20,14 @@ public class WebAttack : Pathfinder {
 
     public void Run(Component initialComponent) {
         Init(initialComponent);
-        this.lifeTicks = 5;
-        this.port = 22;
-        FindAttackableGameObject();
+        AttackComponent(FindAttackableGameObject((Component) GetComponent(typeof(Computer))));
     }
 
-    /// <summary>
-    /// Finds a game object to attack
-    /// </summary>
-    private void FindAttackableGameObject() {
-        while (!this.isAttackable && this.stopScript == false) {
-            if (ScanComponent(SelectedComponent)) {
-                this.isAttackable = true;
-            } else {
-                MoveToNextOutput();
-            }
-        }
-        if (this.stopScript == false) {
-            StartCoroutine(StartAttackingTimer());
-        }
-    }
-
-    private IEnumerator StartAttackingTimer() {
-        Debug.Log(SelectedComponent.name + " is under attack!");
-        while (this.lifeTicks > 0) {
-            yield return new WaitForSeconds(2.0f);
-            if (SelectedComponent.Durability > 0) {
-                AttackComponent();
-            } else {
-                DeleteAttack();
-            }
-        }
-        DeleteAttack();
-    }
-
-    private void AttackComponent() {
+    private void AttackComponent(Component component) {
         // Each attack does 10 dmg to the durability
-        SelectedComponent.Durability -= 10;
-        this.lifeTicks--;
+        component.Durability -= 10;
         Debug.Log("Durability left: " + SelectedComponent.Durability);
         Debug.Log("Lifeticks: " + this.lifeTicks);
         // TODO Update Module panel with new Computer Durability Values
     }
-
-    /// <summary>
-    /// deletes this attack object
-    /// </summary>
-    private void DeleteAttack() {
-        Debug.Log("Deleting attack..");
-        this.stopScript = true;
-        Destroy(this.gameObject);
-    }
-
-    /// <summary>
-    /// Scans the specified component and returns true if computer component
-    /// is found and false if not
-    /// </summary>
-    /// <param name="componentToScan"></param>
-    /// <returns>true if computer component is found and false if not</returns>
-    private bool ScanComponent(Component componentToScan) {
-        bool computerComponentFound = false;
-        try {
-            if (componentToScan.GetType() == typeof(Computer)) {
-                computerComponentFound = true;
-            } else if (componentToScan.GetType() == typeof(Firewall)) {
-                //TODO Change this check to find the status of port in real firewall script (Wait for liban to finish firewall)
-                Firewall firewall = (Firewall) componentToScan;
-                bool portStatus = firewall.GetPort(this.port).IsActive;
-                Debug.Log("Selected component is a firewall, port status: " + portStatus);
-                if (!portStatus) {
-                    if (componentToScan.GetType() == typeof(Computer)) {
-                        computerComponentFound = true;
-                    }
-                } else {
-                    computerComponentFound = false;
-                    DeleteAttack();
-                }
-            } else {
-                Debug.Log("Component is of type: " + componentToScan.Name);
-            }
-        } catch (NullReferenceException nre) {
-            Debug.LogException(nre);
-        }
-        return computerComponentFound;
-    }
-
-    /// <summary>
-    /// Returns the name of the attack. Could be domain, location etc.
-    /// </summary>
-    /// <returns>Returns the name of the attack</returns>
-    public string GetWebAttackName() {
-        return this.name;
-    }
-
-
-
 }
